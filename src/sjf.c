435fe7043dcee2495d00750aee190dab975604b5
“CL4-Bisk” <apariciojohnclyde@gmail.com>
2026-03-15 12:56:39 +0800

    feat: added header include in sjf.c

--- src/sjf.c
+++ src/sjf.c
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../include/gantt.h"
+#include "../include/scheduler.h"
+#include "../include/metrics.h"
 
 int schedule_sjf(SchedulerState *state)
 {

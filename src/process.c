547ffa6a4b5b23c216ceba6613f32931f77300f4
“CL4-Bisk” <apariciojohnclyde@gmail.com>
2026-03-15 12:55:00 +0800

    feat: added load process function in process.c

--- src/process.c
+++ src/process.c
@@ -5,5 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../include/process.h"
 
 // Additional functions for process management can be implemented here
+Process *load_processes(const char *filename, int *num_processes)
+{
+    /*
+     * Implementation to load processes from a file
+     * This function reads process data from a specified file and returns an array of Process structs.
+     * The number of processes loaded is stored in the variable pointed to by num_processes.
+     */
+
+    return NULL; // Placeholder return
+}

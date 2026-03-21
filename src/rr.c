ae25ff5dd9f2c7af1fdcf0c2d38538975e3fe1af
“CL4-Bisk” <apariciojohnclyde@gmail.com>
2026-03-21 22:49:15 +0800

    feat: initialized implementation to round robin scheduler

--- src/rr.c
+++ src/rr.c
@@ -12,6 +12,36 @@
 int schedule_rr(SchedulerState *state, int quantum)
 {
     // Implementation for Round-robin scheduling
+    int time = 0;
+    int completed = 0;
+    int remaining_times[state->num_processes];
 
+    for (int i = 0; i < state->num_processes; i++)
+    {
+        remaining_times[i] = state->processes[i].burst_time;
+    }
+
+    while (completed < state->num_processes)
+    {
+        for (int i = 0; i < state->num_processes; i++)
+        {
+            Process *proc = &state->processes[i];
+            if (proc->arrival_time <= time && remaining_times[i] > 0)
+            {
+                if (remaining_times[i] > quantum)
+                {
+                    time += quantum;
+                    remaining_times[i] -= quantum;
+                }
+                else
+                {
+                    time += remaining_times[i];
+                    proc->finish_time = time;
+                    remaining_times[i] = 0;
+                    completed++;
+                }
+            }
+        }
+    }
     return 0;
 }

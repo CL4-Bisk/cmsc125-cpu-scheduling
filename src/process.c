8d3b71200d748356892dbbb5469263655f0f1bb0
FakeThird <thirdwheelerforhire@gmail.com>
2026-03-23 15:04:40 +0800

    fix: transferred sorter from util to process file

--- src/process.c
+++ src/process.c
@@ -88,6 +88,20 @@ void reset_processes(Process *processes, int num_processes) {
     }
 }
 
+void sort_processes(Process *processes, int num_processes)
+{
+    // Sort processes according to arrival time
+    for (int i = 0; i < num_processes - 1; i++) {
+        for (int j = 0; j < num_processes - i - 1; j++) {
+            if (processes[j].arrival_time > processes[j+1].arrival_time) {
+                Process temp = processes[j];
+                processes[j] = processes[j+1];
+                processes[j+1] = temp;
+            }
+        }
+    }
+}
+
 void print_process(Process proc)
 {
 	printf("Process %s:\n", proc.pid);

2ad2d6f28986c92c6715b0ed8f7a8902349f4128
FakeThird <thirdwheelerforhire@gmail.com>
2026-03-25 12:57:20 +0800

    feat: Added gantt chart printing in stcf

--- src/stcf.c
+++ src/stcf.c
@@ -66,6 +66,7 @@
 
                 if (next_process != -1)
                 {
+                    gantt_add_or_extend(state, "IDLE", time, state->processes[next_process].arrival_time);
                     time = state->processes[next_process].arrival_time; // Move time forward to the next process arrival
                 }
                 else
@@ -89,6 +90,7 @@
             last_pid[15] = '\0';
 
             proc->remaining_time--;
+            gantt_add_or_extend(state, proc->pid, time, time + 1);
             time++;
 
             if (proc->remaining_time == 0)
@@ -98,5 +100,6 @@
             }
         }
         calculate_metrics(state);
+        gantt_print(state);
         return 0;
     }
